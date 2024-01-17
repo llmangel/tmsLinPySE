deb2d0065bd9f53ee4ac3dcde4a449d16f17b850
llmangel <llm19991117@gmail.com>
2024-01-17 14:01:43 +0800

    first

---
+++ src/TestTms.py
@@ -0,0 +1,25 @@
+import time
+import TmsFunc as t
+from time import sleep
+import datetime
+import dis
+import this
+'''
+    tmsInit()->bool
+    tmsLinMasterInit()->bool
+    tmsBreak()->bool
+    tmsMasterSend(frameId: int, data: list)->list
+    tmsMasterRead(frameId: int)->list
+    tmsClose()->bool
+'''
+
+
+
+
+if __name__ == '__main__':
+    # print(t.tmsInit())
+    # print(t.tmsLinMasterInit())
+    # print(t.tmsBreak())
+    # print(t.tmsClose())
+    dis.dis(add)
+    dis.dis(dis.dis)
